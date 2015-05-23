c11d3060f95f442622cfb4f2678dee15b0e0cd1d
Deyan Levski <didolevski@gmail.com>
2015-05-24 00:03:11 +0200

    Updates with camera functionality, initial tests

---
+++ V2-2015/dev/camera/capture.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "Starting image capture"
+fswebcam -d /dev/video0 -r 640x480 --title "Live weather conditions in Ruse, Bulgaria" --jpeg 100 conditions_lores.jpg
+fswebcam -d /dev/video0 -r 1920x1080 --title "Live weather conditions in Ruse, Bulgaria" --jpeg 100 conditions_hires.jpg
+
